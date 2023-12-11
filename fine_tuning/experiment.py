924fa3801b8812dfadf4a38deaa07dfcee8619dc
genki1201 <nishigenki@icloud.com>
2023-12-11 15:35:36 +0900

    commit

--- fine_tuning/experiment.py
+++ fine_tuning/experiment.py
@@ -3,17 +3,17 @@ from PIL import Image
 import numpy as np
 
 # 保存したモデルのパス
-max_model_path = "D:\project_assignment\simple_temp_model\max_model.pth"
+max_model_path = "D:\\project_assignment\\finetuning_model\\max_model.pth"
 
 # モデルを読み込む
 max_model = torch.jit.load(max_model_path, map_location='cpu')
 max_model.eval()
 
-min_model_path = "D:\project_assignment\simple_temp_model\min_model.pth"
+min_model_path = "D:\\project_assignment\\finetuning_model\\min_model.pth"
 min_model = torch.jit.load(min_model_path, map_location='cpu')
 min_model.eval()
 
-img_path= "D:\\project_assignment\\temp_label\\a.jpg"
+img_path= "D:/project_assignment/temp_image/img_small/skirt_creap/114.jpg"
 
 img = Image.open(img_path)
 img = img.convert('RGB')
